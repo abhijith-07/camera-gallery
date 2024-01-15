3cf4c4b0cf38745f9dbf07027b7967cf0a64cd4a
Abhijith Subash <abhijithsubash07@gmail.com>
2024-01-15 16:35:13 +0530

    fix: delete function added

--- gallery.js
+++ gallery.js
@@ -15,10 +15,14 @@ if(db) {
             mediaElem.innerHTML = 
             `<div class="media-container">
                 <div class="media"><video autoplay loop src=${videoURL}></video></div>
-                <div class="delete">Delete</div>
+                <div class="delete" id="${videoObj.videoID}">Delete</div>
                 <div class="download"><a href=${videoURL} download="video.mp4">Download</a></div>
             </div>`         
             galleryContainer.appendChild(mediaElem)   
+
+            // Delete Video
+            let deleteBtn = mediaElem.querySelector(".delete")
+            deleteBtn.addEventListener("click", deleteListener) 
         })
     })
 
@@ -37,11 +41,31 @@ if(db) {
             mediaElem.innerHTML = 
             `<div class="media-container">
                 <div class="media"><img src="${imgURL}" /></div>
-                <div class="delete">Delete</div>
+                <div class="delete" id=${imageObj.imageID}>Delete</div>
                 <div class="download"><a href=${imgURL} download="img.png">Download</a></div>
             </div>`         
-            galleryContainer.appendChild(mediaElem)   
+            galleryContainer.appendChild(mediaElem)  
+
+            // Delete Function
+            let deleteBtn = mediaElem.querySelector(".delete")
+            deleteBtn.addEventListener("click", deleteListener) 
         })
     })
 }
 }, 100)
+
+function deleteListener(e) {
+    let id = e.target.getAttribute("id")
+    if (id.slice(0, 3) == 'img') {
+        let imgDbTransaction = db.transaction("image", "readwrite")
+        let imageStore = imgDbTransaction.objectStore("image")
+        imageStore.delete(id)
+        e.target.parentElement.remove()
+    }
+    else if (id.slice(0, 3) == 'vid') {
+        let dbTransaction = db.transaction("video", "readwrite")
+        let videoStore = dbTransaction.objectStore("video")
+        videoStore.delete(id)
+        e.target.parentElement.remove()
+    }
+}
